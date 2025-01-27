f827b49abf649ed5bd336d94f6c2111137dba148
abdullah mostafa <abdullahmostafa737@gmail.com>
2025-01-27 23:33:35 +0200

    add promise to nodemailer

--- src/utils/email/email.js
+++ src/utils/email/email.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer"
 
 
 // async..await is not allowed in global scope, must use a wrapper
-export const sendEmail = ({ to = "", cc = "", bcc = "", subject = "Confirm-Email", text = "", html = "", attachments = "" } = {}) => {
+export const sendEmail = async ({ to = "", cc = "", bcc = "", subject = "Confirm-Email", text = "", html = "", attachments = "" } = {}) => {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         auth: {
@@ -11,17 +11,37 @@ export const sendEmail = ({ to = "", cc = "", bcc = "", subject = "Confirm-Email
             pass: process.env.EMAIL_PASS,
         },
     });
+    await new Promise((resolve, reject) => {
+        transporter.verify((error, success) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(success)
+            }
+        })
+    })
     // send mail with defined transport object
-    const info = transporter.sendMail({
-        from: `"Saraha App" <${process.env.EMAIL}>`, // sender address
-        to, // list of receivers
-        cc,
-        bcc,
-        subject, // Subject line
-        text, // plain text body
-        html,
-        attachments // html body
-    });
+    await new Promise((resolve, reject) =>{
+        const info = transporter.sendMail({
+            from: `"Saraha App" <${process.env.EMAIL}>`, // sender address
+            to, // list of receivers
+            cc,
+            bcc,
+            subject, // Subject line
+            text, // plain text body
+            html,
+            attachments // html body
+        }, (err, info)=>{
+            if(err)
+            {
+                reject(err)
+            }else{
+                resolve(info)
+            }
+            
+        });
+    })
+
 
     console.log("Message sent: %s", info.messageId);
     // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
--- src/utils/events/sendEmail.js
+++ src/utils/events/sendEmail.js
@@ -11,7 +11,7 @@ emailEvent.on("sendEmail", async (data)=>{
     const emailToken = generateToken({payload: {email}, signature: process.env.EMAIL_SINGNATURE})
     const emailLink = `${process.env.FE_URL}/confirmEmail/${emailToken}`
     const html = generateEmailTemplates(emailLink)
-    sendEmail({to:email, subject:"Confirm Email", html})
+    await sendEmail({to:email, subject:"Confirm Email", html})
 
 })
 
