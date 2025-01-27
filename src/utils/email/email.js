ba9874bb3db60831c896b9c6fd1109a0620554db
abdullah mostafa <abdullahmostafa737@gmail.com>
2025-01-28 01:18:00 +0200

    add changes

--- src/utils/email/email.js
+++ src/utils/email/email.js
@@ -4,21 +4,27 @@ import nodemailer from "nodemailer"
 
 // async..await is not allowed in global scope, must use a wrapper
 export const sendEmail = async ({ to = "", cc = "", bcc = "", subject = "Confirm-Email", text = "", html = "", attachments = "" } = {}) => {
-    return new Promise((resolve, reject) => {
-
-        const transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.EMAIL_PASS,
-            },
-            pool: true,
-            debug: true,
-            logger: true
-        });
-
-        // send mail with defined transport object
-
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASS,
+        },
+        pool: true,
+        debug: true,
+        logger: true
+    });
+    await new Promise((resolve, reject) => {
+        transporter.verify((error, success) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(success)
+            }
+        })
+    })
+    // send mail with defined transport object
+    await new Promise((resolve, reject) =>{
         const info = transporter.sendMail({
             from: `"Saraha App" <${process.env.EMAIL}>`, // sender address
             to, // list of receivers
@@ -28,23 +34,19 @@ export const sendEmail = async ({ to = "", cc = "", bcc = "", subject = "Confirm
             text, // plain text body
             html,
             attachments // html body
-        }, (err, info) => {
-            if (err) {
+        }, (err, info)=>{
+            if(err)
+            {
                 reject(err)
-            } else {
+            }else{
                 resolve(info)
             }
-
+            
         });
-
-
-
-        console.log("Message sent: %s", info.messageId);
-
-
-
     })
 
+
+    console.log("Message sent:");
     // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
 }
 
