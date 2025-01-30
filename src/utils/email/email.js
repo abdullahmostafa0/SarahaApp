d246ceef799a301f07ff6b3f2db1ed2f32f4ac04
abdullah mostafa <abdullahmostafa737@gmail.com>
2025-01-30 17:33:15 +0200

    user nodemailer to send email

--- src/utils/email/email.js
+++ src/utils/email/email.js
@@ -4,44 +4,25 @@ import { Resend } from 'resend';
 
 // async..await is not allowed in global scope, must use a wrapper
 export const sendEmail = async ({ to = "", cc = "", bcc = "", subject = "Confirm-Email", text = "", html = "", attachments = "" } = {}) => {
-    /*const transporter = nodemailer.createTransport({
+    const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port:465,
-        secure:true,
         auth: {
             user: process.env.EMAIL,
             pass: process.env.EMAIL_PASS,
         },
-        pool: true
     });
-    
     // send mail with defined transport object
-    
-        const info = transporter.sendMail({
-            from: `"Saraha App" <${process.env.EMAIL}>`, // sender address
-            to, // list of receivers
-            cc,
-            bcc,
-            subject, // Subject line
-            text, // plain text body
-            html,
-            attachments // html body
-        
-    })
-    
-
-    console.log("Message sent:");
-    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-
-*/
-    const resend = new Resend('re_jEUgDbt6_GAsdpqShkyybm8JsaPCLiMBK');
-
-    await resend.emails.send({
-        from: "onboarding@resend.dev",
-        to : to,
-        subject: subject,
-        html: html,
+    const info = transporter.sendMail({
+        from: `"Social Media App" <${process.env.EMAIL}>`, // sender address
+        to, // list of receivers
+        cc,
+        bcc,
+        subject, // Subject line
+        text, // plain text body
+        html,
+        attachments // html body
     });
-    console.log("message sent")
+
+    console.log("Message sent: %s", info.messageId);
 }
 
